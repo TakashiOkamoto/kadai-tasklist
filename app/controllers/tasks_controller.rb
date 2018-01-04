f007f43aa62ff26dce6b888e05466541f0909f4f
Tabo <techtest.takashi1227@gmail.com>
2018-01-04 13:55:06 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,6 +41,11 @@ class TasksController < ApplicationController
 
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
   
 end
