d19632c8ba3297e3a9216972770d712c2fcc4e10
Tabo <techtest.takashi1227@gmail.com>
2018-01-04 13:30:11 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,17 @@ class TasksController < ApplicationController
   end
 
   def update
+     @task = Task.find(params[:id])
+
+     if @task.update(task_params)
+       flash[:success] = 'Task は正常に更新されました'
+       redirect_to @task
+     else
+       flash.now[:danger] = 'Task は更新されませんでした'
+       render :edit
+     end
   end
+
 
   def destroy
   end
@@ -37,5 +47,5 @@ end
 
 # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
